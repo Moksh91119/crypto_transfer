a1253726c9525fedd460fd5f69b239c048d24efc
Moksh Jain <111447162+Moksh91119@users.noreply.github.com>
2024-02-16 12:10:45 +0530

    Update Transactions.jsx

--- front_end/src/components/Transactions.jsx
+++ front_end/src/components/Transactions.jsx
@@ -2,11 +2,9 @@ import React, { useContext } from "react";
 
 import { TransactionContext } from "../context/TransactionContext";
 
-import useFetch from "../hooks/useFetch";
 import { shortenAddress } from "../utils/shortenAddress";
 
 const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
-  const gifUrl = useFetch({ keyword });
 
   return (
     <div className="bg-[#181918] m-4 flex flex-1
@@ -33,11 +31,6 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
             </>
           )}
         </div>
-        <img
-          src={gifUrl || url}
-          alt="nature"
-          className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
-        />
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
           <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
